68c15938d809a632c2a7079371e6f1bfea2aeeb5
Shayan Patel <zlac405@live.rhul.ac.uk>
2025-07-27 18:42:18 +0100

    Send email

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.header("Contact Us")
+
+with st.form(key="email_forms"):
+    user_email = st.text_input("Email")
+    message = st.text_area("Your Message")
+    button = st.form_submit_button()
+    if button:
+        message = message + "\n" + user_email
+        
